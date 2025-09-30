8724e7da643a68972ac16da4d362b09b22ae27ba
ZahaAnass <zahaanass277@gmail.com>
2025-09-30 15:24:09 +0100

    add all routes with middlewares with connection to mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,54 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+// Load environment variables
 dotenv.config();
 
+// Initialize app
 const app = express();
 const port = process.env.PORT || 3000;
 
+// connectDB
+const connectDB = require("./config/connect")
+
 // Middleware
-app.use(cors());
+const authenticateUser = require("./middleware/authMiddleware")
+const notFoundMiddleware = require("./middleware/notFound")
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
-// Routes placeholder
+// Home route
 app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-}).then(() => {
-  console.log("✅ MongoDB Connected...");
-  app.listen(port, () => {
-    console.log(`🚀 Server running on port ${port}`);
-  });
+  res.send("<h1>Alpha Chat</h1>")
 })
-.catch((err) => console.error(err));
+
+// routers
+const authRouter = require("./routes/auth")
+const userRouter = require("./routes/user")
+const roomRouter = require("./routes/room")
+const messageRouter = require("./routes/message")
+
+// Routes
+app.use("/api/auth", authRouter)
+app.use("/api/user", authenticateUser, userRouter)
+app.use("/api/room", authenticateUser, roomRouter)
+app.use("/api/message", authenticateUser, messageRouter)
+
+// Error handler
+app.use(notFoundMiddleware)
+
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`🚀 Server running on port ${port}`);
+    });
+  }catch(err){
+    console.log(err)
+  }
+}
+
+start();
