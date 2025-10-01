7537a8b739ae2c4334d6666e70c30487139626d4
ZahaAnass <zahaanass277@gmail.com>
2025-10-01 16:59:04 +0100

    update routes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { getUserInfo, joinRoom, leaveRoom, sendJoinRequest, updateUserInfo } = require("../controllers/user")
+const { getUserInfo, leaveRoom, sendJoinRequest, updateUserInfo } = require("../controllers/user")
 
-router.get("/", getUserInfo)
-router.post("/join-room", joinRoom)
+router.route("/").get(getUserInfo).post(updateUserInfo)
 router.post("/leave-room", leaveRoom)
 router.post("/send-join-request", sendJoinRequest)
-router.post("/update-user-info", updateUserInfo)
 
 module.exports = router
